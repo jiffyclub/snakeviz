02a854f418f6d8a5fdc6e6a845af149b73c369dd
Matt Davis <jiffyclub.programatic@gmail.com>
2012-07-07 11:47:18 -0400

    Added an upload form and figured out how to get access to a pstats profile file.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os.path
+import pstats
+import StringIO
 
 import tornado.ioloop
 import tornado.web
@@ -8,13 +10,25 @@ import tornado.web
 import handler
 
 settings = {
-    'static_path': os.path.join(os.path.dirname(__file__), "static"),
+    'static_path': os.path.join(os.path.dirname(__file__), 'static'),
     'debug': True
 }
 
 class MainHandler(handler.Handler):
     def get(self):
         self.render('main.html')
+
+    def post(self):
+        with open(self.request.files['profile'][0]['filename'], 'wb') as f:
+            f.write(self.request.files['profile'][0]['body'])
+
+        s = StringIO.StringIO()
+
+        stats = pstats.Stats(self.request.files['profile'][0]['filename'], stream=s)
+        stats.print_stats()
+
+        self.request.headers['Content-Type'] = 'text/plain'
+        self.write(s.getvalue())
 
 handlers = [(r'/', MainHandler)]
 

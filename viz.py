c8428d3064a95cc10cdbbf5c4822183810c2cb48
Matt Davis <jiffyclub.programatic@gmail.com>
2012-08-16 22:31:30 -0400

    Added sweet tables showing the profile data below the sunburst visualization.

--- viz.py
+++ viz.py
@@ -1,6 +1,25 @@
-import handler
+import pstats
+import StringIO
 
+import handler
+import upload
 
 class SunburstHandler(handler.Handler):
     def get(self, profile_name):
-        self.render('sunburst.html', profile_name=profile_name)
+        # get the print_stats output from pstats
+        # sorted with most cumulative time at the top
+        stats_stream = StringIO.StringIO()
+        stats = pstats.Stats(upload.storage_name(profile_name),
+            stream=stats_stream)
+        stats.strip_dirs()
+        stats.sort_stats('cum').print_stats()
+
+        # get just the table rows, none of the header or footer
+        data = stats_stream.getvalue().strip()
+        key_str = 'filename:lineno(function)\n'
+        data_index = data.find(key_str) + len(key_str)
+
+        rows = data[data_index:].split('\n')
+        rows = [r.split() for r in rows]
+
+        self.render('sunburst.html', profile_name=profile_name, stats_rows=rows)

f3a95f8f98dc45b63d6512921f725cbb57100f63
Matt Davis <jiffyclub.programatic@gmail.com>
2012-09-15 18:33:18 -0400

    Added docstrings and comments to upload.py.

--- websnakeviz/upload.py
+++ websnakeviz/upload.py
@@ -1,3 +1,10 @@
+"""
+This module contains the handlers for the upload page and the JSON
+request URL. In the standalone, command line version the upload handler
+is not used.
+
+"""
+
 import pstats
 import json
 import tempfile
@@ -9,10 +16,32 @@ import handler
 
 
 def storage_name(filename):
+    """
+    Prepend the temporary file directory to the input `filename`.
+
+    Parameters
+    ----------
+    filename : str
+        Any name to give a file.
+
+    Returns
+    -------
+    tempname : str
+        `filename` with temporary file directory prepended.
+
+    """
+    if len(filename) == 0:
+        raise ValueError('filename must length greater than 0.')
+
     return os.path.join(tempfile.gettempdir(), filename)
 
 
 class UploadHandler(handler.Handler):
+    """
+    Handler for a profile upload page. Not used in the command line
+    version.
+
+    """
     def get(self):
         self.render('upload.html')
 
@@ -39,6 +68,10 @@ class UploadHandler(handler.Handler):
 
 
 class JSONHandler(handler.Handler):
+    """
+    Handler for requesting the JSON representation of a profile.
+
+    """
     def get(self, prof_name):
         if self.request.path.startswith('/json/file/'):
             if self.settings['single_user_mode']:
@@ -46,11 +79,14 @@ class JSONHandler(handler.Handler):
                     prof_name = '/' + prof_name
                 filename = os.path.abspath(prof_name)
             else:
-                # TODO: Raise a 404 error here
-                pass
+                self.send_error(status_code=404)
         else:
             filename = storage_name(prof_name)
 
+        # Use multiprocessing.Pool here so that there can be a timeout
+        # on the call to prof_to_json.
+        # This will give prof_to_json 10 seconds to return and if it doesn't
+        # it will shut down the process and return None
         pool = mp.Pool(1, maxtasksperchild=1)
         result = pool.apply_async(prof_to_json, (filename,))
         pool.close()
@@ -67,15 +103,55 @@ class JSONHandler(handler.Handler):
 
 
 def prof_to_json(prof_name):
+    """
+    Convert profiles stats in a `pstats` compatible file to a JSON string.
+
+    Parameters
+    ----------
+    prof_name : str
+        Path to to a `pstats` compatible profile.
+
+    Returns
+    -------
+    json_stats : str
+        Profile as a JSON string.
+
+    """
     loader = pstatsloader.PStatsLoader(prof_name)
 
-    d = stats_to_tree_dict(loader.tree.children[0])
+    d = _stats_to_tree_dict(loader.tree.children[0])
 
     return json.dumps(d, indent=1)
 
 
-def stats_to_tree_dict(node, parent=None, parent_size=None,
-                       recursive_seen=None):
+def _stats_to_tree_dict(node, parent=None, parent_size=None,
+                        recursive_seen=None):
+    """
+    `_stats_to_tree_dict` is a specialized function for converting
+    a `pstatsloader.PStatsLoader` profile representation into a tree
+    of nested dictionaries by recursively calling itself.
+    It is primarily meant to be called from `prof_to_json`.
+
+    Parameters
+    ----------
+    node : `pstatsloader.PStatsRow` or `pstatsloader.PStatGroup`
+        One node of the call tree.
+    parent : `pstatsloader.PStatsRow` or `pstatsloader.PStatGroup`
+        Parent of `node`. Optional for the root node.
+    parent_size : float
+        Calculated size of `parent`. Optional for the root node.
+    recursive_seen : set
+        Set of nodes that are direct ancestors of `node`.
+        This is used to prevent `_stats_to_tree_dict` from ending up in
+        infinite loops when it encounters recursion.
+        Optional for the root node.
+
+    Returns
+    -------
+    tree_dict : dict
+        Tree of nested dictionaries representing the profile call tree.
+
+    """
     # recursive_seen prevents us from repeatedly traversing
     # recursive structures. only want to show the first set.
     if recursive_seen is None:
@@ -99,22 +175,29 @@ def stats_to_tree_dict(node, parent=None, parent_size=None,
         recursive_seen.add(node)
 
     if parent:
+        # figure out the size of this node. This is an arbitrary value
+        # but it's important that the child size is no larger
+        # than the parent size.
         if isinstance(parent, pstatsloader.PStatGroup):
             if parent.cummulative:
                 d['size'] = node.cummulative / parent.cummulative * parent_size
             else:
+                # this is a catch-all when it's not possible
+                # to calculate a size. hopefully this doesn't come
+                # up too often.
                 d['size'] = 0
         else:
             d['size'] = parent.child_cumulative_time(node) * parent_size
     else:
+        # default size for the root node
         d['size'] = 1000
 
     if node.children:
         d['children'] = []
         for child in node.children:
             if child not in recursive_seen:
-                child_dict = stats_to_tree_dict(child, node, d['size'],
-                                                recursive_seen)
+                child_dict = _stats_to_tree_dict(child, node, d['size'],
+                                                 recursive_seen)
                 d['children'].append(child_dict)
 
         if d['children']:
@@ -143,9 +226,13 @@ def stats_to_tree_dict(node, parent=None, parent_size=None,
 
                 d['children'].append(d_internal)
         else:
+            # there were no non-recursive children so get rid of the
+            # children list.
             del d['children']
 
     if node in recursive_seen:
+        # remove this node from the set so it doesn't interfere if this
+        # node shows up again in another part of the call tree.
         recursive_seen.remove(node)
 
     return d

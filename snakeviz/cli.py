d6afe8e2f890bd7be8900f190b290aae65df460b
Tony S Yu <tsyu80@gmail.com>
2014-11-02 15:26:28 -0600

    Switch from optparse to argparse

--- snakeviz/cli.py
+++ snakeviz/cli.py
@@ -1,42 +1,41 @@
+#!/usr/bin/env python
 """
 This module contains the command line interface for snakeviz.
 
 """
 from __future__ import print_function
 
-import optparse
+import argparse
 import os
-import sys
 import threading
 import webbrowser
 
 
-def main(argv=sys.argv[1:]):
-    parser = optparse.OptionParser(
-        usage='%prog [options] filename'
-    )
-    parser.add_option('-H', '--hostname', metavar='ADDR', default='127.0.0.1',
-                      help='hostname to bind to (default: 127.0.0.1')
+def main():
+    formatter = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=formatter)
+    parser.add_argument('filename', help="Python profile statistics.")
+    parser.add_argument('-H', '--hostname',
+                        metavar='ADDR', default='127.0.0.1',
+                        help='hostname to bind to (default: 127.0.0.1')
 
     # TODO: Make this help text actually true
-    parser.add_option('-p', '--port', type='int', metavar='PORT', default=8080,
-                      help='port to bind to; if this port is already in use a'
-                           'free port will be selected automatically '
-                           '(default: %default)')
+    parser.add_argument('-p', '--port', type=int,
+                        metavar='PORT', default=8080,
+                        help="port to bind to; if this port is already in use "
+                             "a free port will be selected automatically "
+                             "(default: %default)")
 
-    parser.add_option('-b', '--browser', metavar='PATH',
-                      help="path to the web browser executable to use to open "
-                           "the visualization; uses the same default as "
-                           "Python's webbrowser module, which can also be "
-                           "overridden with the BROWSER environment variable")
+    parser.add_argument('-b', '--browser', metavar='PATH',
+                        help="path to the web browser executable used to open "
+                             "open the visualization; uses same default as "
+                             "Python's webbrowser module, which can also be "
+                             "overridden with BROWSER environment variable")
 
-    options, args = parser.parse_args(argv)
+    args = parser.parse_args()
 
-    if len(args) != 1:
-        parser.error('please provide the path to a profiler output file to '
-                     'open')
-
-    filename = os.path.abspath(args[0])
+    filename = os.path.abspath(args.filename)
     if not os.path.exists(filename):
         parser.error('the file %s does not exist' % filename)
 
@@ -46,15 +45,15 @@ def main(argv=sys.argv[1:]):
         parser.error('the file %s could not be opened: %s'
                      % (filename, str(e)))
 
-    hostname = options.hostname
-    port = options.port
+    hostname = args.hostname
+    port = args.port
 
     if not 0 <= port <= 65535:
         parser.error('invalid port number %d: use a port between 0 and 65535'
                      % port)
 
     try:
-        browser = webbrowser.get(options.browser)
+        browser = webbrowser.get(args.browser)
     except webbrowser.Error as e:
         parser.error('no web browser found: %s' % e)
 
@@ -88,5 +87,3 @@ def main(argv=sys.argv[1:]):
         # stuff for handling SIGINT and SIGTERM that might be worth borrowing
         tornado.ioloop.IOLoop.instance().stop()
         print('\nBye!')
-
-    return 0

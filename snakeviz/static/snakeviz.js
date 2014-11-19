31a48df8268aefd2dcb56f021c84e897219d77c5
Matt Davis <jiffyclub@gmail.com>
2014-11-18 16:21:12 -0800

    fix bug that stalled things when clicking on a leaf node

--- snakeviz/static/snakeviz.js
+++ snakeviz/static/snakeviz.js
@@ -79,7 +79,7 @@ function sv_build_heirarchy(
             data['children'].push({
                 name: root_name,
                 display_name: data['display_name'],
-                parent_name: root_name,
+                parent_name: data['parent_name'],
                 cumulative: stats[root_name]['stats'][3],
                 size: Math.max(0, (parent_time - time_in_children) / parent_time * node_size)
             });

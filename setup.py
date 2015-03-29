96fb498b212e1b98883bb82928c0958dad1d88e6
Matt Davis <jiffyclub@gmail.com>
2015-03-28 22:22:55 -0700

    add python 3 versions to pypi classifiers
    [ci skip]

--- setup.py
+++ setup.py
@@ -42,6 +42,8 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development'
     ],
     packages=['snakeviz'],

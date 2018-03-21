264d8c006d6349873024d32e1c03e7002328ab89
Jay Young <github@yjmade.net>
2018-03-21 14:43:29 +0800

    Check if %snakeviz magic is running in jupyter notebook, then embed the visualization inside the notebook as a html cell #58 #88

--- snakeviz/ipymagic.py
+++ snakeviz/ipymagic.py
@@ -1,8 +1,17 @@
 import subprocess
 import tempfile
 import time
+from IPython.display import display, HTML
+import uuid
+import urllib
 
 __all__ = ['load_ipython_extension']
+
+
+JUPYTER_HTML_TEMPLATE = """
+<iframe id='{uuid}' frameborder=0 seamless width='100%%' height='1000'></iframe>
+<script>$("#{uuid}").attr({{src:"http://"+document.location.hostname+":{port}{path}"}})</script>
+"""
 
 
 def snakeviz_magic(line, cell=None):
@@ -26,8 +35,15 @@ def snakeviz_magic(line, cell=None):
         ip.run_line_magic('prun', line)
 
     # start up a Snakeviz server
-    sv = subprocess.Popen(['snakeviz', filename])
-
+    if _check_ipynb():
+        port = str(_find_free_port())
+        sv = subprocess.Popen(['snakeviz', "-s", "-H", "0.0.0.0", "-p", port, filename])
+        time.sleep(0.5)
+        path = "/snakeviz/%s" % urllib.parse.quote_plus(filename)
+        # data = requests.get(url).content
+        display(HTML(JUPYTER_HTML_TEMPLATE.format(port=port, path=path, uuid=uuid.uuid1())))
+    else:
+        sv = subprocess.Popen(['snakeviz', filename])
     # give time for the Snakeviz page to load then shut down the server
     time.sleep(3)
     sv.terminate()
@@ -36,3 +52,16 @@ def snakeviz_magic(line, cell=None):
 def load_ipython_extension(ipython):
     ipython.register_magic_function(snakeviz_magic, magic_kind='line_cell',
                                     magic_name='snakeviz')
+
+
+def _check_ipynb():
+    cfg = get_ipython().config
+    return "connection_file" in cfg["IPKernelApp"]
+
+
+def _find_free_port():
+    import socket
+    from contextlib import closing
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]

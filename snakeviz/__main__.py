708ab9fb69f835649cad581912034c68b1eec940
Matt Davis <jiffyclub@gmail.com>
2018-08-04 14:40:57 -0700

    Wrap CLI call in sys.exit for status codes

--- snakeviz/__main__.py
+++ snakeviz/__main__.py
@@ -4,4 +4,4 @@ from .cli import main
 if __name__ == "__main__":
     # __main__.py is ugly and confusing, monkey patch executable to say snakeviz
     sys.argv[0] = "snakeviz"
-    main()
+    sys.exit(main())

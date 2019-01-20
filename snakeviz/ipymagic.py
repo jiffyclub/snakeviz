f61189f3ed85069af291c09e0d86a153e65dcb95
Matt Davis <jiffyclub@gmail.com>
2019-01-19 21:08:38 -0800

    Add -e/--embed option to snakeviz magic
    So that if the user is running the SnakeViz magic in the
    notebook they can choose to have it embedded instead of
    opened in a new tab.

--- snakeviz/ipymagic.py
+++ snakeviz/ipymagic.py
@@ -1,9 +1,11 @@
 import subprocess
 import tempfile
 import time
-from IPython.display import display, HTML
 import uuid
 import urllib
+
+from IPython.core.magic import Magics, magics_class, line_cell_magic
+from IPython.display import display, HTML
 
 __all__ = ['load_ipython_extension']
 
@@ -14,42 +16,69 @@ JUPYTER_HTML_TEMPLATE = """
 """
 
 
-def snakeviz_magic(line, cell=None):
-    """
-    Profile code and display the profile in Snakeviz.
-    Works as a line or cell magic.
+@magics_class
+class SnakevizMagic(Magics):
 
-    """
-    # get location for saved profile
-    filename = tempfile.NamedTemporaryFile().name
+    @line_cell_magic
+    def snakeviz(self, line, cell=None):
+        """
+        Profile code and display the profile in Snakeviz.
+        Works as a line or cell magic.
 
-    # call signature for prun
-    line = '-q -D ' + filename + ' ' + line
+        Usage, in line mode:
+          %snakeviz [options] statement
 
-    # generate the stats file using IPython's prun magic
-    ip = get_ipython()
+        Usage, in cell mode:
+          %%snakeviz [options] [statement]
+          code...
+          code...
 
-    if cell:
-        ip.run_cell_magic('prun', line, cell)
-    else:
-        ip.run_line_magic('prun', line)
+        Options:
 
-    # start up a Snakeviz server
-    if _check_ipynb():
-        sv = open_snakeviz_and_display_in_notebook(filename)
-    else:
-        sv = subprocess.Popen(['snakeviz', filename])
-    # give time for the Snakeviz page to load then shut down the server
-    time.sleep(3)
-    sv.terminate()
+        -e/--embed
+          If running the snakeviz magic in the Jupyter Notebook,
+          use this flag to embed the snakeviz visualization within
+          the notebook instead of trying to open a new tab.
+
+        """
+        # get location for saved profile
+        filename = tempfile.NamedTemporaryFile().name
+
+        # parse options
+        opts, line = self.parse_options(line, 'e', 'embed', posix=False)
+
+        # call signature for prun
+        line = '-q -D ' + filename + ' ' + line
+
+        # generate the stats file using IPython's prun magic
+        ip = get_ipython()
+
+        if cell:
+            ip.run_cell_magic('prun', line, cell)
+        else:
+            ip.run_line_magic('prun', line)
+
+        # start up a Snakeviz server
+        if _check_ipynb() and ('e' in opts or 'embed' in opts):
+            sv = open_snakeviz_and_display_in_notebook(filename)
+        else:
+            sv = subprocess.Popen(['snakeviz', filename])
+        # give time for the Snakeviz page to load then shut down the server
+        time.sleep(3)
+        sv.terminate()
 
 
 def load_ipython_extension(ipython):
-    ipython.register_magic_function(snakeviz_magic, magic_kind='line_cell',
-                                    magic_name='snakeviz')
+    """Called when user runs %load_ext snakeviz"""
+    ipython.register_magics(SnakevizMagic)
 
 
 def _check_ipynb():
+    """
+    Returns True if IPython is running as the backend for a
+    Jupyter Notebook.
+
+    """
     cfg = get_ipython().config
     return "connection_file" in cfg["IPKernelApp"]
 
@@ -69,19 +98,17 @@ def open_snakeviz_and_display_in_notebook(filename):
         import os
         environ = os.environ.copy()
         environ["PYTHONUNBUFFERED"] = "TRUE"
-        sv = subprocess.Popen(['snakeviz', "-s", "-H", "0.0.0.0", "-p", port, filename],
-                              stdout=subprocess.PIPE, universal_newlines=True, env=environ)
-        # import datetime
-        # now = datetime.datetime.now()
+        sv = subprocess.Popen(
+            ['snakeviz', "-s", "-p", port, filename],
+            stdout=subprocess.PIPE, universal_newlines=True, env=environ)
         while True:
             line = sv.stdout.readline()
             if line.strip().startswith("snakeviz web server started"):
                 break
-        # print("wait", (datetime.datetime.now() - now).total_seconds())
         return sv
 
     sv = _start_and_wait_when_ready()
     path = "/snakeviz/%s" % urllib.parse.quote_plus(filename)
-    # data = requests.get(url).content
-    display(HTML(JUPYTER_HTML_TEMPLATE.format(port=port, path=path, uuid=uuid.uuid1())))
+    display(HTML(JUPYTER_HTML_TEMPLATE.format(
+        port=port, path=path, uuid=uuid.uuid1())))
     return sv

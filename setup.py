7b5795f0e23389221708957e56ed31ff80a2aa5a
Matthew Feickert <matthew.feickert@cern.ch>
2021-09-27 17:54:29 -0500

    docs: Add Python 3.8, 3.9 to PyPI metadata

--- setup.py
+++ setup.py
@@ -41,6 +41,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development'
     ],
     packages=['snakeviz'],

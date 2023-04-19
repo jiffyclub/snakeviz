02f34d2ae12c3e67c074f34a2934628f7c12ab4f
Dave Bacon <dabacon@google.com>
2023-04-19 11:40:07 -0700

    remove extra init code

--- snakeviz/__init__.py
+++ snakeviz/__init__.py
@@ -1,6 +1,2 @@
-import os
-
-with open(os.path.join('snakeviz', 'version.py'), 'w') as f:
-    f.write('__version__ = version = %r' % "2.2")
 from .version import __version__
 from .ipymagic import *
